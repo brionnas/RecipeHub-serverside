db5c605faedf68ba76e85c0456b75edcdec30384
brionnas <brionnaswinton@gmail.com>
2025-06-17 23:28:34 -0400

    added post request

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const Joi = require('joi');
 const recipes = require('./recipe');
 
 const app = express();
@@ -9,20 +10,46 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-// Route to serve index.html
+// Serve static index page
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// API route to get all recipes
+// GET all recipes
 app.get('/api/recipes', (req, res) => {
   res.json(recipes);
+});
+
+// Joi schema matching your format
+const recipeSchema = Joi.object({
+  title: Joi.string().required(),
+  description: Joi.string().required(),
+  prep_time: Joi.number().min(1).required(),
+  rating: Joi.number().min(0).max(5).required(),
+  img_name: Joi.string().required()
+});
+
+// POST new recipe
+app.post('/api/recipes', (req, res) => {
+  const { error } = recipeSchema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ error: error.details[0].message });
+  }
+
+  const newRecipe = {
+    _id: recipes.length + 1,
+    ...req.body
+  };
+
+  recipes.push(newRecipe);
+  res.status(201).json({ success: true, recipe: newRecipe });
 });
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
 
 
 
